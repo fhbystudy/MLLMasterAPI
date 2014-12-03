d3f9484e182b0c4129bdf67a4339f93cc8bef604
haobo <haobo@meilele.com>
2014-12-03 09:24:41 +0800

    add example folder

---
+++ Specs/MLLMasterAPI/2.0.0/Examples/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MLLMasterAPI Example
+//
+//  Created by Tonny.hao on 11/27/14.
+//  Copyright (c) 2014 Tonny.hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
